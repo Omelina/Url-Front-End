9f57468878d23364ba1219b19a3251779a103aa6
Oky Melina <okymelina98@gmail.com>
2021-10-02 21:40:39 -0600

    fixing errors

--- client/src/app/login/login.component.ts
+++ client/src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'login',
@@ -7,6 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  loginForm = new FormGroup({
+      email : new FormControl('', Validators.required),
+      password : new FormControl('', Validators.required)
+    })
+    
   constructor() { }
 
   ngOnInit(): void {
